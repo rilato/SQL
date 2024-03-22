824831a5e5806cc8e35cd0e59ed8b4b6d47ae74a
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-22 22:27:08 +0900

    Create ^연도_별_평균_미세먼지_농도_조회하기.sql

---
+++ STRING,_DATE/^연도_별_평균_미세먼지_농도_조회하기.sql
@@ -0,0 +1,25 @@
+-- 코드를 작성해주세요
+-- YEAR 함수는 정수로 반환
+-- DATE_FORMAT 함수는 문자열로 반환
+-- 아마, 문제에서 정수 타입을 원했거나, 문자열을 정렬할 때 문제가 발생한 것으로 보임
+
+-- 틀린 풀이
+
+SELECT DATE_FORMAT(YM, '%Y') AS 'YEAR',
+    ROUND(AVG(PM_VAL1), 2) AS 'PM10',
+    ROUND(AVG(PM_VAL2), 2) AS 'PM2.5'
+FROM AIR_POLLUTION
+WHERE LOCATION2 = '수원'
+GROUP BY YEAR
+ORDER BY YEAR;
+
+
+-- 맞은 풀이
+
+SELECT YEAR(YM) AS 'YEAR',
+    ROUND(AVG(PM_VAL1), 2) AS 'PM10',
+    ROUND(AVG(PM_VAL2), 2) AS 'PM2.5'
+FROM AIR_POLLUTION
+WHERE LOCATION2 = '수원'
+GROUP BY YEAR
+ORDER BY YEAR;
