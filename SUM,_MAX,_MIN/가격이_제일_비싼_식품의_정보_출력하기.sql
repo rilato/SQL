5285f529a2ee8fe1761a4adc854efa203327fb74
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-22 20:36:18 +0900

    Update 가격이_제일_비싼_식품의_정보_출력하기.sql

--- SUM,_MAX,_MIN/가격이_제일_비싼_식품의_정보_출력하기.sql
+++ SUM,_MAX,_MIN/가격이_제일_비싼_식품의_정보_출력하기.sql
@@ -4,3 +4,17 @@ SELECT *
 FROM FOOD_PRODUCT
 WHERE PRICE = (SELECT MAX(PRICE)
                FROM FOOD_PRODUCT);
+
+
+
+
+-- 참고
+
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, MAX(PRICE) AS PRICE
+FROM FOOD_PRODUCT
+
+-- 해당 쿼리는 가격이 가장 비싼 row 를 가져올 것 같지만
+-- 테이블의 첫번째 행 데이터에 가격만 MAX값으로 합쳐서 나오는 것입니다.
+
+-- 먼저 MAX(PRICE) 값을 SELECT 하시고
+-- WHERE 절에서 PRICE가 최대값인지 찾으시면 되겠습니다.
