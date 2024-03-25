c3fa560ffed1ca1e2de65cd7e5713ed3a499c0be
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-25 21:29:15 +0900

    Update ^즐겨찾기가_가장_많은_식당_정보_출력하기.sql

--- GROUP_BY/^즐겨찾기가_가장_많은_식당_정보_출력하기.sql
+++ GROUP_BY/^즐겨찾기가_가장_많은_식당_정보_출력하기.sql
@@ -1,10 +1,11 @@
 -- GROUP BY는 MAX를 커버하지 못한다.
--- https://jaehwaseo.tistory.com/24
+-- SUM/MAX/MIN의 물고기 종류 별 대어 찾기 문제와 동일한 형식의 문제
+
+-- 코드를 입력하세요
 
 SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
 FROM REST_INFO
-WHERE (FOOD_TYPE, FAVORITES) IN 
-    (SELECT FOOD_TYPE, MAX(FAVORITES)
-        FROM REST_INFO
-        GROUP BY FOOD_TYPE)
+WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES) 
+                                FROM REST_INFO
+                                GROUP BY FOOD_TYPE)
 ORDER BY FOOD_TYPE DESC;
