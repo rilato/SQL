7ef4f81faefb707722217df8929863cca5628287
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-26 19:39:21 +0900

    Create ^조건에_맞는_사용자_정보_조회하기.sql

---
+++ STRING,_DATE/^조건에_맞는_사용자_정보_조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+-- 문제를 꼼꼼이 읽자. 시 까지 포함한다.
+
+SELECT U.USER_ID,
+    U.NICKNAME,
+    CONCAT(U.CITY, " ", U.STREET_ADDRESS1, " " , U.STREET_ADDRESS2) AS '전체주소',
+    CONCAT(SUBSTRING(TLNO,1,3), '-', SUBSTRING(TLNO,4,4), '-', SUBSTRING(TLNO,8,4)) AS '전화번호'
+FROM USED_GOODS_BOARD B
+    JOIN USED_GOODS_USER U
+    ON B.WRITER_ID = U.USER_ID
+WHERE B.WRITER_ID IN (SELECT WRITER_ID
+                      FROM USED_GOODS_BOARD
+                      GROUP BY WRITER_ID
+                      HAVING COUNT(WRITER_ID) >= 3)
+GROUP BY U.USER_ID
+ORDER BY 1 DESC;
