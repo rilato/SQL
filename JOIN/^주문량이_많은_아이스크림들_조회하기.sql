a00662a09b717244ea0f9b0b1ece142b9de56edb
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-28 19:13:51 +0900

    Update ^주문량이_많은_아이스크림들_조회하기.sql

--- JOIN/^주문량이_많은_아이스크림들_조회하기.sql
+++ JOIN/^주문량이_많은_아이스크림들_조회하기.sql
@@ -1,5 +1,15 @@
--- 코드를 입력하세요
--- JULY 테이블은 FLAVOR가 중복되어 나타날 수 있다. 이를 계산해주어야 한다.
+-- 틀린 풀이
+-- SHIPMENT_ID가 없다면 JOIN하지 않기 때문에, JOIN된 테이블에서 FLAVOR별로 JULY의 총합을 구할 수 없어, 잘못된 풀이이다.
+
+SELECT F.FLAVOR
+FROM FIRST_HALF F
+    JOIN JULY J
+    ON F.SHIPMENT_ID = J.SHIPMENT_ID
+GROUP BY J.FLAVOR
+ORDER BY F.TOTAL_ORDER + SUM(J.TOTAL_ORDER) DESC
+LIMIT 3;
+
+-- 맞은 풀이
 
 SELECT F.FLAVOR
 FROM FIRST_HALF F
