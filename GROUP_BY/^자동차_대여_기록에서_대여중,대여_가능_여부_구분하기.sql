6ec790eac15ae22171a468e5ffc501a0949635fe
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-28 21:25:26 +0900

    Update ^자동차_대여_기록에서_대여중,대여_가능_여부_구분하기.sql

--- GROUP_BY/^자동차_대여_기록에서_대여중,대여_가능_여부_구분하기.sql
+++ GROUP_BY/^자동차_대여_기록에서_대여중,대여_가능_여부_구분하기.sql
@@ -1,4 +1,3 @@
--- 코드를 입력하세요
 -- SUM을 이용한 트릭을 쓴다.
 -- 하나라도 10월 16일에 걸리면, 해당 차는 대여중인 것이다.
 -- => 값이 1 이상이 된다.
@@ -6,6 +5,16 @@
 -- 자동차별로 대여 가능한지 출력하므로, GROUP BY CAR_ID를 해준다.
 
 SELECT CAR_ID, IF(SUM(IF('2022-10-16' BETWEEN DATE_FORMAT(START_DATE, "%Y-%m-%d") AND DATE_FORMAT(END_DATE, "%Y-%m-%d"), 1, 0)) = 0, "대여 가능", "대여중") AS AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY 1 DESC;
+
+-- 좀 더 보기 좋은 코드
+SELECT CAR_ID,
+    IF(SUM(
+        IF('2022-10-16' BETWEEN DATE_FORMAT(START_DATE, "%Y-%m-%d")
+                AND DATE_FORMAT(END_DATE, "%Y-%m-%d"), 1, 0)
+       ) = 0, "대여 가능", "대여중") AS AVAILABILITY
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
 GROUP BY CAR_ID
 ORDER BY 1 DESC;
