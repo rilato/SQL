69d130335df5efccff15e185e4da35ab54413120
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-28 23:04:45 +0900

    Update and rename ^자동차_대여_기록에서_장기_단기_대여_구분하기.sql to 자동차_대여_기록에서_장기,단기_대여_구분하기.sql

--- STRING,_DATE/자동차_대여_기록에서_장기,단기_대여_구분하기.sql
+++ STRING,_DATE/자동차_대여_기록에서_장기,단기_대여_구분하기.sql
@@ -1,4 +1,3 @@
--- 코드를 입력하세요
 -- DATEDIFF를 사용한다. 이 때, DATEDIFF에 들어가는 형식에 주의한다.
 -- 또한, 9월 1일 ~ 9월 1일 => 차이는 0일이지만, 실제로는 1일 빌렸다.
 -- 따라서 계산 시, DATE_DIFF 값에 1을 더해준다.
@@ -11,3 +10,15 @@ FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
 WHERE YEAR(START_DATE) = 2022
     AND MONTH(START_DATE) = 9
 ORDER BY HISTORY_ID DESC;
+
+
+-- 다른 풀이
+
+SELECT HISTORY_ID,
+       CAR_ID,
+       DATE_FORMAT(START_DATE, "%Y-%m-%d") AS START_DATE,
+       DATE_FORMAT(END_DATE, "%Y-%m-%d") AS END_DATE,
+       IF(DATEDIFF(END_DATE, START_DATE) >= 29, "장기 대여", "단기 대여") AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE DATE_FORMAT(START_DATE, "%Y-%m") = '2022-09'
+ORDER BY 1 DESC;
