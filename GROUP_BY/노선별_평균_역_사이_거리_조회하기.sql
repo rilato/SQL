96d85cb6179c97340b04c050c7fe61c100603da1
Yunsik Lee <leeyunsik1997@g.hongik.ac.kr>
2024-03-30 11:52:10 +0900

    Update and rename ^노선별_평균_역_사이_거리_조회하기.sql to 노선별_평균_역_사이_거리_조회하기.sql

--- GROUP_BY/노선별_평균_역_사이_거리_조회하기.sql
+++ GROUP_BY/노선별_평균_역_사이_거리_조회하기.sql
@@ -1,5 +1,5 @@
--- 코드를 작성해주세요
--- total_distance 가 concat 함수를 써서 문자열로 되었기 때문에, 제대로 정렬이 안되는 거에요. order by route가 아니라, round(sum(d_between_dist), 1) desc로 하면, 숫자를 기준으로 정렬을 하니까 정상적으로 통과할거에요.
+-- total_distance 가 concat 함수를 써서 문자열로 되었기 때문에, 제대로 정렬이 안됨.
+-- order by route가 아니라, round(sum(d_between_dist), 1) desc로 하면, 숫자를 기준으로 정렬을 하므로 옳은 풀이
 
 -- 틀린 풀이
 
